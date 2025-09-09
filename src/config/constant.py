92ba8016bbae273f8ecf32f205a667aa54780fe6
AGS Lakpahana <agslakpahana@gmail.com>
2025-09-09 21:05:11 +0530

    change consts

--- src/config/constant.py
+++ src/config/constant.py
@@ -15,23 +15,14 @@ from pyrogram import Client, types
 class BotText:
 
     start = """
-    Welcome to YouTube Download bot. Type /help for more information.
-    EU🇪🇺: @benny_2ytdlbot
-    SG🇸🇬：@benny_ytdlbot
-
-    Join https://t.me/ytdlbot0 for updates.\n\n"""
+    Welcome to Manali YouTube Download bot. Type /help for more information.
+    Contact @sanla for updates.\n\n"""
 
     help = """
-1. For YouTube and any websites supported by yt-dlp, just send the link and we will engine and send it to you.
-
-2. For specific links use `/spdl {URL}`. More info at https://github.com/tgbot-collection/ytdlbot#supported-websites 
-
-3. If the bot doesn't work, try again or join https://t.me/ytdlbot0 for updates.
-
-4. Want to deploy it yourself?\nHere's the source code: https://github.com/tgbot-collection/ytdlbot
+    @sanla
     """
 
-    about = "YouTube Downloader by @BennyThink.\n\nOpen source on GitHub: https://github.com/tgbot-collection/ytdlbot"
+    about = "YouTube Downloader by @sanla.\n\nOpen source on GitHub: https://github.com/lakpahana/ytdlbot"
 
     settings = """
 Please choose the preferred format and video quality for your video. These settings only **apply to YouTube videos**.
